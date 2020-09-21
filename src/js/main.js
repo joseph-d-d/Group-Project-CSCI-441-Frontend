3de6b78f6d16d96818f0a2e94e56b443dd05a776
Tony B <25971310+MrBanh@users.noreply.github.com>
2020-09-20 20:21:45 -0700

    refactor: Switch out cdn links/scripts with the .min files and remove nav-link to 'about' page

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,8 @@
 });*/
 
 // Selected elements
-const navLinks = document.querySelectorAll(".nav-link");
+
 
 // Event handlers
-function removeActive () {
-	navLinks.forEach(function (navLink) {
-		navLink.classList.remove("active");
-	});
-}
-
-function addActive (el) {
-	el.classList.add("active");
-}
 
 // Event Listeners
-navLinks.forEach((navLink) => {
-	navLink.addEventListener("click", function (event) {
-		removeActive();
-		addActive(event.target);
-	});
-});
