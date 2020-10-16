8b08a4d80664f20a0514234e0462a981279cd265
Tony B <25971310+MrBanh@users.noreply.github.com>
2020-10-16 04:06:41 -0700

    feat: Add dynamic loading of different sections of account settings based on the account settings selected

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,10 +1,11 @@
 // Example user
 const user = {
     _id: "507f191e810c19729de860ea",
-    email: "John.Smith@example.com",
-    firstName: "",
-    lastName: "",
+    email: "Tony@example.com",
+    firstName: "Tony",
+    lastName: "Banh",
     password: "test1234",
+    phone: "408-123-4567",
     paymentMethods: [{
         cardName: "John Smith",
         cardNumber: "123456789111111",
@@ -18,4 +19,52 @@ const user = {
     ]
 }
 
-console.log(JSON.stringify(user));
+$(".emailAddress").text(user.email);
+
+function clearActive() {
+    $(".active").removeClass("active");
+}
+
+function setActive(el) {
+    el.classList.add("active");
+}
+
+function updateForm(setting) {
+    $(".form_title").text(setting);
+    const parentElement = $(".sidebar_info");
+
+    switch (setting) {
+        case "My Profile":
+            showMyProfile(parentElement, user);
+            $(parentElement).append(`<button type="button" class="btn btn-outline-primary btn-lg btn_update">Update</button>`)
+
+            break;
+        case "My Vehicles":
+            showMyVehicles(parentElement, user.vehicles);
+            break;
+        case "My Payment Methods":
+            showMyPaymentMethod(parentElement, user.paymentMethods[0]);
+            break;
+        case "My Reservations":
+            // showMyReservations(parentElement, reservations);
+            $(parentElement).text("Coming soon...");
+            break;
+        default:
+            console.error("Error");
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    updateForm($(".active").find(".sidebar_text").text().trim());
+});
+
+$(".list-group-item").click(function (event) {
+    event.preventDefault();
+    clearActive();
+    setActive(this);
+    updateForm($(this).find(".sidebar_text").text().trim());
+})
+
+$(".signOut").click(function (event) {
+    // TODO: un-authenticate user
+})
